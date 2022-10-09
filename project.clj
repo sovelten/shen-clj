250a14259ad96fd4ccc4f045371722b258bfd1f5
Sophia Velten <sophia.velten@nubank.com.br>
2022-10-08 22:17:27 -0300

    fix project

--- project.clj
+++ project.clj
@@ -3,11 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.analyzer.jvm "0.7.2"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.analyzer.jvm "1.2.2"]
                  [org.clojure/core.match "0.3.0-alpha5"]
-                 [midje "1.9.2-alpha2"]
-                 [nubank/matcher-combinators "0.2.4"]
+                 [midje "1.10.5"]
                  [instaparse "1.4.8"]]
   :plugins [[lein-midje "3.2.1"]]
   :main ^:skip-aot shen-port.install
